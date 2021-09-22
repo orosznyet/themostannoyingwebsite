d44b0b4c4a8e7b7369e672364a1521ed8ac67de5
Jozsef Koller <orosznyet@gmail.com>
2021-09-22 19:10:48 +0200

    chore(*): add editor config and eslintrc.

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: [
+        'alloy',
+        'alloy/react',
+        'alloy/typescript',
+    ],
+    env: {
+        browser: true,
+        es2021: true
+    },
+    globals: {},
+    rules: {},
+};
