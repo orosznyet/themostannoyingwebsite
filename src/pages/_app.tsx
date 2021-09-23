b6d312def1af7735c1f853d56c3e500e79b39a8b
Jozsef Koller <orosznyet@gmail.com>
2021-09-23 12:49:09 +0200

    feat(stats): add matomo trial implementation.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,46 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import MainLayout from '@/components/MainLayout';
+import { MatomoProvider, createInstance, useMatomo } from '@datapunt/matomo-tracker-react';
+import { useEffect } from 'react';
+
+const instance = createInstance({
+  urlBase: 'https://themostannoyingwebsite.matomo.cloud',
+  siteId: 1,
+  disabled: false,
+  heartBeat: {
+    active: true,
+    seconds: 10,
+  },
+  linkTracking: true,
+  configurations: {
+    disableCookies: true,
+    setSecureCookie: true,
+    setRequestMethod: 'POST',
+  },
+});
 
 const TheMostAnnoyingWebsite = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  const { trackPageView } = useMatomo();
+
+  useEffect(() => {
+    trackPageView({});
+    router.events.on('routeChangeComplete', (route: string) => {
+      trackPageView({
+        // documentTitle: document.title,
+        // href: window.location.href,
+      });
+    });
+  }, []);
+
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <MatomoProvider value={instance}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </MatomoProvider>
   );
 };
 
