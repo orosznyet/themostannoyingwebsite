acdb49a810ed5d5ace68fdf2f9612fcff06758db
Jozsef Koller <orosznyet@gmail.com>
2021-09-23 13:58:00 +0200

    chore(*): add extra info to dev-ssl console output.

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.prepare().then(() => {
     handle(req, res, parsedUrl);
   }).listen(3000, (err) => {
     if (err) throw err;
-    console.log('> Server started on https://localhost:3000');
+    console.log('> Server started on https://localhost:3000 using self-signed cert.');
   });
 });
