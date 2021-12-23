30f662975412d9a0debf54f0314d42547a63415f
Jozsef Koller <orosznyet@gmail.com>
2021-12-23 10:47:42 +0100

    feature(theme): add device breakpoints and media queries.

--- src/components/master/Theme.tsx
+++ src/components/master/Theme.tsx
@@ -11,6 +11,42 @@ import { createGlobalStyle } from "styled-components"
 // For more info:
 // https://material.io/design/color/the-color-system.html#color-theme-creation
 
+export const breakpoints = {
+  xs: "0",
+  sm: "576px",
+  md: "768px",
+  lg: "992px",
+  xl: "1200px",
+  xxl: "1400px"
+}
+
+const cssRulePrefix = '@media screen and';
+const cssRuleMinMax =
+  (min: string, max: string) => `(min-width: ${min}) and (max-width: ${max})`;
+export const cssRule = {
+  // Strictly targeted dimensions
+  xs: `${cssRulePrefix} ${cssRuleMinMax(breakpoints.xs, breakpoints.sm)}`,
+  sm: `${cssRulePrefix} ${cssRuleMinMax(breakpoints.sm, breakpoints.md)}`,
+  md: `${cssRulePrefix} ${cssRuleMinMax(breakpoints.md, breakpoints.lg)}`,
+  lg: `${cssRulePrefix} ${cssRuleMinMax(breakpoints.lg, breakpoints.xl)}`,
+  xl: `${cssRulePrefix} ${cssRuleMinMax(breakpoints.xl, breakpoints.xxl)}`,
+  xxl: `${cssRulePrefix} (min-width: ${breakpoints.xxl})`,
+  // Targeting without uppper limit
+  xsUp: `${cssRulePrefix} (min-width: ${breakpoints.xs})`,
+  smUp: `${cssRulePrefix} (min-width: ${breakpoints.sm})`,
+  mdUp: `${cssRulePrefix} (min-width: ${breakpoints.md})`,
+  lgUp: `${cssRulePrefix} (min-width: ${breakpoints.lg})`,
+  xlUp: `${cssRulePrefix} (min-width: ${breakpoints.xl})`,
+  xxlUp: `${cssRulePrefix} (min-width: ${breakpoints.xxl})`,
+  // Targeting without lower limit
+  xsDown: `${cssRulePrefix} (max-width: ${breakpoints.sm})`,
+  smDown: `${cssRulePrefix} (max-width: ${breakpoints.md})`,
+  mdDown: `${cssRulePrefix} (max-width: ${breakpoints.lg})`,
+  lgDown: `${cssRulePrefix} (max-width: ${breakpoints.xl})`,
+  xlDown: `${cssRulePrefix} (max-width: ${breakpoints.xxl})`,
+  xxlDown: `${cssRulePrefix}`,
+}
+
 const DarkTheme = createGlobalStyle`
   :root {
     --color-background: #050416;
