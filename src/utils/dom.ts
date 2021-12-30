1251e6eb0ad8d8f805840cb91fbb8ba797d214bc
Jozsef Koller <orosznyet@gmail.com>
2021-12-30 15:43:37 +0100

    feature(utils): add angle, distance and point within calculator.

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,7 +1,19 @@
+import { Point } from "./math"
+
 export const getRelativePosition = (element: HTMLElement, target: HTMLElement) => {
   const rect = element.getBoundingClientRect()
   const targetRect = target.getBoundingClientRect()
   const x = targetRect.left - rect.left
   const y = targetRect.top - rect.top
   return { x, y }
+}
+
+export const isPointWithin = (element: HTMLElement, point: Point) => {
+  const rect = element.getBoundingClientRect()
+  return (
+    point.x >= rect.left &&
+    point.y >= rect.top &&
+    point.x <= rect.right &&
+    point.y <= rect.bottom
+  )
 }
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,21 +1,32 @@
-export function random(min: number, max: number): number {
+export const random = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-export function clamp(value: number, min: number, max: number): number {
+export const clamp = (value: number, min: number, max: number): number => {
   return Math.min(Math.max(value, min), max)
 }
 
 export type Point = { x: number, y: number }
-export function midpoint(p1: Point, p2: Point): Point {
+export const midpoint = (p1: Point, p2: Point): Point => {
   return {
     x: (p1.x + p2.x) / 2,
     y: (p1.y + p2.y) / 2,
   }
 }
 
-export function radToDeg(rad: number): number {
+export const radToDeg = (rad: number): number => {
   return rad * 180 / Math.PI
+}
+
+export const angleRad = (p1: Point, p2: Point): number => {
+  return Math.atan2(p2.y - p1.y, p2.x - p1.x)
+}
+
+export const distance = (p1: Point, p2: Point): Point => {
+  return {
+    x: p1.x - p2.x,
+    y: p1.y - p2.y,
+  }
 }
 
 export const getWeightedRandom = <T>(items: {value: T, prob: number}[]): T | undefined => {
